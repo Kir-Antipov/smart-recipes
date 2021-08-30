f9a85e569bb851fbf1418b8bcaa51b402099d3b0
Kir_Antipov <kp.antipov@gmail.com>
2021-08-30 16:26:23 +0300

    Made mod initializer

---
+++ src/main/java/dev/kirantipov/smartrecipes/SmartRecipes.java
@@ -0,0 +1,22 @@
+package dev.kirantipov.smartrecipes;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SmartRecipes implements ClientModInitializer {
+    public static final String MOD_ID = "smart_recipes";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public static Identifier locate(String id) {
+        return new Identifier(MOD_ID, id);
+    }
+
+    @Override
+    @Environment(EnvType.CLIENT)
+    public void onInitializeClient() {
+    }
+}
